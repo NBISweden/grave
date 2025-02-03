0eff067daddf91e33a51307ed44d075d7459aecc
CormacKinsella <27350062+CormacKinsella@users.noreply.github.com>
2025-02-03 23:01:06 +0100

    housekeeping

--- example-cluster-job-script.sh
+++ example-cluster-job-script.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -l
 
-# Go to grave repository (update path)
+# Go to grave repository (edit path as needed)
 
 cd ~/grave
 
-# Run grave in the background (edit options for your needs)
+# Run grave in the background (edit options as needed)
 
 tmux new -s grave-run -d /bin/bash -c "pixi run nextflow main.nf --account "naiss2024-22-619" -profile dardelSlurm"
 
 echo "Grave pipeline started in the background"
 echo "You can monitor the progress in '.nextflow.log' and with 'squeue'"
+

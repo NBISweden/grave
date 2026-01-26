930a3867918f3833db2ca64660a62caa7ac3949f
CormacKinsella <27350062+CormacKinsella@users.noreply.github.com>
2026-01-26 14:44:44 +0100

    draft edit to slurm script

--- bin/slurm_example.sh
+++ bin/slurm_example.sh
@@ -2,45 +2,45 @@
 
 # EDIT THESE VALUES
 
+PIPELINE_NAME=grave
 REPO_PATH=~/grave
 TMUX_SESSION_NAME=grave
-PIXI_COMMAND="pixi run -e default nextflow main.nf --graphDir ./data/test --samplesheet ./data/test/samplesheet.csv --pathsDir ./data/test/paths --account SLURM_PROJECT_CODE -profile apptainer,dardelSlurm"
-PIPELINE_NAME=grave
+PIXI_COMMAND="pixi run nextflow main.nf -profile pdc_kth -params-file params.yml --project <SLURM_ALLOCATION>"
 
 ### DO NOT EDIT BELOW THIS LINE ###
 
 if [ -d "$REPO_PATH" ]; then
 
-	cd $REPO_PATH
+    cd $REPO_PATH
 
-	TMUX_VERSION_OUTPUT=$(tmux -V 2>&1)
+    TMUX_VERSION_OUTPUT=$(tmux -V 2>&1)
 
-	if [ $? -ne 0 ]; then
-		echo "Error: tmux not found"
-	else
-		if echo "$TMUX_VERSION_OUTPUT" | grep -q "^tmux "; then
-			MAJORVERSION=$(echo "$TMUX_VERSION_OUTPUT" | sed 's/^tmux //;s/\..*$//')
-			if [ "$MAJORVERSION" -eq 1 ]; then
-				tmux new-session -s "$TMUX_SESSION_NAME" -d
-				tmux send-keys -t "$TMUX_SESSION_NAME" "$PIXI_COMMAND" C-m
-				echo "Found tmux version $MAJORVERSION"
-				echo "$PIPELINE_NAME pipeline started in tmux session: '$TMUX_SESSION_NAME', with command:"
-				echo "$PIXI_COMMAND"
-				echo "Monitor workflow progress in '.nextflow.log' or with 'squeue -u $USER'"
-			elif [ "$MAJORVERSION" -ge 2 ]; then
-				tmux new -s "$TMUX_SESSION_NAME" -d /bin/bash -c "$PIXI_COMMAND"
-				echo "Found tmux version $MAJORVERSION"
-				echo "$PIPELINE_NAME pipeline started in tmux session: '$TMUX_SESSION_NAME', with command:"
-				echo "$PIXI_COMMAND"
-				echo "Monitor workflow progress in '.nextflow.log' or with 'squeue -u $USER'"
-			fi
-		else
-			echo "Error: Unable to parse tmux version"
-		fi
-	fi
+    if [ $? -ne 0 ]; then
+        echo "Error: tmux not found"
+    else
+        if echo "$TMUX_VERSION_OUTPUT" | grep -q "^tmux "; then
+            MAJORVERSION=$(echo "$TMUX_VERSION_OUTPUT" | sed 's/^tmux //;s/\..*$//')
+            if [ "$MAJORVERSION" -eq 1 ]; then
+                tmux new-session -s "$TMUX_SESSION_NAME" -d
+                tmux send-keys -t "$TMUX_SESSION_NAME" "$PIXI_COMMAND" C-m
+                echo "Found tmux version $MAJORVERSION"
+                echo "$PIPELINE_NAME pipeline started in tmux session: '$TMUX_SESSION_NAME', with command:"
+                echo "$PIXI_COMMAND"
+                echo "Monitor workflow progress in '.nextflow.log' or with 'squeue -u $USER'"
+            elif [ "$MAJORVERSION" -ge 2 ]; then
+                tmux new -s "$TMUX_SESSION_NAME" -d /bin/bash -c "$PIXI_COMMAND"
+                echo "Found tmux version $MAJORVERSION"
+                echo "$PIPELINE_NAME pipeline started in tmux session: '$TMUX_SESSION_NAME', with command:"
+                echo "$PIXI_COMMAND"
+                echo "Monitor workflow progress in '.nextflow.log' or with 'squeue -u $USER'"
+            fi
+        else
+            echo "Error: Unable to parse tmux version"
+        fi
+    fi
 
 else
 
-	echo "Error: '$REPO_PATH' does not exist, set to the $PIPELINE_NAME repository path."
+    echo "Error: '$REPO_PATH' does not exist, set to the $PIPELINE_NAME repository path."
 
 fi
